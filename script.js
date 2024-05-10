3014b14d5acafea2033a77e60a6aa59b425b98ea
Marlie Martinez <marlieahue@gmail.com>
2024-05-10 14:27:13 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//part 1 of the lab
+//fizz buzz
+//looping through all numbers from 1-100
+//if number divisble by 3, the function should console.log("fizz")
+//if a number is divisible by 5, it should console "buzz"
+// if divisible by both 3 && 5, it should print FizzBUzz
+//every other number, should just print out as is
+
